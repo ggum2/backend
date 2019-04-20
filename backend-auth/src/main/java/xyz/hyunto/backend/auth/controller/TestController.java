3758ed9e49e65c9e0bd305cb9c802d8ba19c4766
hyunto <jhyunto@gmail.com>
2019-04-21 02:35:27 +0900

    [#1] Gradle Multi Project 구성
    - core, auth 프로젝트 생성
    - 빌드 의존성 추가 : core 빌드에 auth가 추가되도록 설정
    - 테스트용 코드 추가

--- backend-auth/src/main/java/xyz/hyunto/backend/auth/controller/TestController.java
+++ backend-auth/src/main/java/xyz/hyunto/backend/auth/controller/TestController.java
@@ -3,10 +3,10 @@ package xyz.hyunto.backend.auth.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "/auth")
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "Hello, backend-auth";
     }
